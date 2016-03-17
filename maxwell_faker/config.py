0463544227dd1080249c1f323d0b4a8b1c188e3b
Nicolas Maquet <nicolas@movio.co>
2016-03-18 11:01:51 +1300

    change config format

--- maxwell_faker/config.py
+++ maxwell_faker/config.py
@@ -1,4 +1,37 @@
 # -*- coding: utf-8 -*-
 
+from utils import usage
+
+def check(message, predicate):
+    if not predicate:
+        usage(message)
+
 def validate_config(config):
-    pass
+
+    # generator section
+    check(
+        'section "generator" is missing',
+        'generator' in config
+    )
+    check(
+        'value "generator.seed" is missing',
+        'seed' in config['generator']
+    )
+
+    # kafka section
+    check(
+        'section "kafka" is missing',
+        'kafka' in config
+    )
+    check(
+        'section "kafka.brokers" is missing',
+        'brokers' in config['kafka']
+    )
+    check(
+        'section "kafka.brokers" should be a list',
+        type(config['kafka']['brokers']) is list
+    )
+    check(
+        'section "kafka.topic" is missing',
+        'topic' in config['kafka']
+    )
